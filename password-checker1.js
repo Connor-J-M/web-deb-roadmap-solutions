66ca73ef49fbd7e8d37a562a9dcb95a6a45acdba
Connor-J-M <146788474+Connor-J-M@users.noreply.github.com>
2026-08-27 20:14:28 -0500

    Create password-checker1.js

---
+++ password-checker1.js
@@ -0,0 +1,61 @@
+function hasMinimumLength(pass){
+  if (pass.length >= 8) {
+    return true 
+  } else {
+    return false
+  }
+}
+
+function hasNumber(pass) {
+  return /\d/.test(pass);
+}
+
+function hasUppercaseLetter(pass) {
+  if (pass.toLowerCase() === pass){
+    return false
+  } else {
+    return true
+  }
+}
+
+function getFailedRules(pass) {
+  failed = []
+  if (hasMinimumLength(pass)) {
+  } else {
+    failed.push(" minimum length");
+  }
+
+  if (hasNumber(pass)){
+  } else {
+    failed.push(" has number");
+  }
+
+  if (hasUppercaseLetter(pass)){
+  } else {
+    failed.push(" no uppercase letter")
+  }
+  return failed;
+}
+
+function validatePassword(pass) {
+  Failed = getFailedRules(pass);
+  Valid = "Valid: ";
+  Check = ""
+  
+  if (Failed.length > 0){
+    Check = "False";
+  } else {
+    Check = "True";
+  }
+
+  Valid = `${Check}`;
+  
+  const validation = {
+    valid: Valid,
+    failed: failed
+  };
+  console.log(validation);
+  return validation;
+};
+
+console.log (validatePassword("duttttoonnn22UI"))  
