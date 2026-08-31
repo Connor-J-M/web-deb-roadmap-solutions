a27a593ed6a03736d10f358bcfb96bf7f0458001
Connor-J-M <146788474+Connor-J-M@users.noreply.github.com>
2026-08-30 20:41:41 -0500

    update custom dropdown

--- custom-dropdown1.js
+++ custom-dropdown1.js
@@ -1,6 +1,17 @@
+//Creates and loops through list of elements in dropdown and uses createOptionListener() to assign each an event listener
+var dropdowns = document.getElementsByClassName("option");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      createOptionListener(openDropdown, i);
+      
+    }
+
+//Toggles list to be visible when option select is clicked
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
@@ -18,17 +29,24 @@ window.onclick = function(event) {
     }
 } 
 
-var dropdowns = document.getElementsByClassName("option");
-    var i;
-    var u = 0;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      createOptionListener(openDropdown, i)
-    }
-
+//Creates event listener for object and assigns inner HTML of object to dropdown button
 function createOptionListener(openDropdown, i) {
   openDropdown.addEventListener("click", function(e){
-    document.getElementById("dropbtn").innerHTML = i + 1;
+    document.getElementById("dropbtn").innerHTML = openDropdown.innerHTML;
+    setSelected(openDropdown);
   })
-  console.log(openDropdown.innerHTML);
+}
+
+//Changes color of text to indicate selected option
+function setSelected(selectedOption) {
+    var dropdowns = document.getElementsByClassName("option");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+       if (openDropdown != selectedOption) {
+    openDropdown.style.color = "black";
+    } else {
+         openDropdown.style.color = "red";
+    }
+  }
 }
